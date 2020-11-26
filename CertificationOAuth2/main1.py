22e43324d8993af66bfa755823e085f23862f671
朱猛 <2068531506@qq.com>
2020-11-27 06:34:10 +0800

    Certication

--- CertificationOAuth2/main1.py
+++ CertificationOAuth2/main1.py
@@ -7,6 +7,7 @@ import xlwt
 import pytesseract
 from PIL import Image
 import json
+from collections import defaultdict
 
 userAgent = ["Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2227.1 Safari/537.36",
       "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36",
@@ -172,7 +173,6 @@ def requestAndGetCookies(agent):
         return (False,"")
     else:
         if result.status_code == 200:
-            print(result.cookies)
             cookies = requests.utils.dict_from_cookiejar(result.cookies)
             return (True,cookies)
         else:
@@ -196,26 +196,101 @@ def getAuthCodeImage(cookies,agent):
     else:
         return True
 
-def dealAuthCode():
-    # 灰度处理
-    img = Image.open("verifycode.jpg")
-    tmpImage = img.convert('L')
+# 获取图片中像素点数量最多的像素
+def get_threshold(image):
+    pixel_dict = defaultdict(int)
 
-    # 二值化处理
-    threshold = 140
+    # 像素及该像素出现次数的字典
+    rows, cols = image.size
+    for i in range(rows):
+        for j in range(cols):
+            pixel = image.getpixel((i, j))
+            pixel_dict[pixel] += 1
+
+    count_max = max(pixel_dict.values()) # 获取像素出现出多的次数
+    pixel_dict_reverse = {v:k for k,v in pixel_dict.items()}
+    threshold = pixel_dict_reverse[count_max] # 获取出现次数最多的像素点
+
+    return threshold
+
+# 按照阈值进行二值化处理
+# threshold: 像素阈值
+def get_bin_table(threshold):
+    # 获取灰度转二值的映射table
     table = []
     for i in range(256):
-        if i < threshold:
-            table.append(0)
-        else:
+        rate = 0.4 # 在threshold的适当范围内进行处理
+        if threshold*(1-rate)<= i <= threshold*(1+rate):
             table.append(1)
-    out = tmpImage.point(table, '1')
+        else:
+            table.append(0)
+    return table
 
-    # 验证码识别
-    result = pytesseract.image_to_string(out)
-    result = ''.join(result.split())
+# 去掉二值化处理后的图片中的噪声点
+def cut_noise(image):
 
-    return result
+    rows, cols = image.size # 图片的宽度和高度
+    change_pos = [] # 记录噪声点位置
+
+    # 遍历图片中的每个点，除掉边缘
+    for i in range(1, rows-1):
+        for j in range(1, cols-1):
+            # pixel_set用来记录该店附近的黑色像素的数量
+            pixel_set = []
+            # 取该点的邻域为以该点为中心的九宫格
+            for m in range(i-1, i+2):
+                for n in range(j-1, j+2):
+                    if image.getpixel((m, n)) != 1: # 1为白色,0位黑色
+                        pixel_set.append(image.getpixel((m, n)))
+
+            # 如果该位置的九宫内的黑色数量小于等于4，则判断为噪声
+            if len(pixel_set) <= 4:
+                change_pos.append((i,j))
+
+    # 对相应位置进行像素修改，将噪声处的像素置为1（白色）
+    for pos in change_pos:
+        image.putpixel(pos, 1)
+
+    return image # 返回修改后的图片
+
+# 识别图片中的数字加字母
+# 传入参数为图片路径，返回结果为：识别结果
+def OCR_lmj(img_path):
+
+    image = Image.open(img_path) # 打开图片文件
+    imgry = image.convert('L')  # 转化为灰度图
+    out = imgry
+
+    (x,y) = imgry.size
+    out = out.resize((x*10,y*10),Image.ANTIALIAS)
+
+    # 获取图片中的出现次数最多的像素，即为该图片的背景
+    max_pixel = get_threshold(imgry)
+
+    # 将图片进行二值化处理
+    # 注意，是否使用二值化要看具体情况，有些图片二值化之后，可能关键信息会丢失，反而识别不出来
+    table = get_bin_table(threshold=max_pixel)
+    out = out.point(table, '1')
+    #out.show()
+
+    # 去掉图片中的噪声（孤立点）
+    #out = cut_noise(out)
+    #out.show()
+
+    #保存图片
+    # out.save('E://figures/img_gray.jpg')
+
+    # 仅识别图片中的数字
+    #text = pytesseract.image_to_string(out, config='digits')
+    # 识别图片中的数字和字母
+    text = pytesseract.image_to_string(out,lang='letter',config="--psm 4 --oem 3 -c tessedit_char_whitelist=qwertyuiopasdfghjklzxcvbnm")
+
+    # 去掉识别结果中的特殊字符
+    exclude_char_list = ' .:\\|\'\"?![],()~@#$%^&*_+-={};<>/¥\n'
+    text = ''.join([x for x in text if x not in exclude_char_list])
+    #print(text)
+
+    return text
 
 
 def getCerticaitionInfo(name,cardId,agent,cookies,authCode):
@@ -233,18 +308,21 @@ def getCerticaitionInfo(name,cardId,agent,cookies,authCode):
               'type': 1 ,
               'random':authCode}
     try:
-        result = requests.post(url,params=params,headers=header,cookies=cookies,timeout=60)
+        result = requests.post(url,data=params,headers=header,cookies=cookies,timeout=60)
     except Exception:
         return (False,"")
     if result.status_code == 200:
         if result.text == '[{error=randomError}]':
             return (False,"")
         else:
-            data = json.dumps(result.text)
+            data = json.loads(result.text)
             if len(data) == 0:
-                return (True,"没有证书")
+                return (True,["没有证书"])
             else:
-                return (True,result.text)
+                resultData = []
+                for tmpData in data:
+                    resultData.append(" ".join(tmpData.values()))
+                return (True,resultData)
     else:
         return (False,"")
 
@@ -259,7 +337,7 @@ def getCert(name,certId,agent):
     if False == authCodeResult:
         return (False,"")
 
-    authCode = dealAuthCode()
+    authCode = OCR_lmj("verifycode.jpg")
 
     result = getCerticaitionInfo(name=name,
                                  cardId=certId,
@@ -277,7 +355,7 @@ if __name__ == "__main__":
             filePath = input("请输入Excel文件路径：")
             print("您输入的文件路径是：", filePath)
             workBook = xlrd.open_workbook(filePath)
-        except FileNotFoundError:
+        except Exception:
             print("文件不存在，请重新输入\n")
         else:
             break
@@ -295,8 +373,8 @@ if __name__ == "__main__":
     for index in range(worksheet.nrows):
 
         print("\n")
-        cardId = str(worksheet.cell(index, 0).value)
-        name = str(worksheet.cell(index, 1).value)
+        name = str(worksheet.cell(index, 0).value)
+        cardId = str(worksheet.cell(index, 1).value)
         print(cardId + " " + name)
 
         retry = 0
@@ -320,14 +398,14 @@ if __name__ == "__main__":
 
         if retry == 5 :
             print("重试结束")
-            cert = "查询失败"
+            cert = ["查询失败"]
 
         outWorksheet.write(index, 0, cardId)
         outWorksheet.write(index, 1, name)
+        for i in range(len(cert)) :
+            outWorksheet.write(index, 2 + i, cert[i])
 
-        outWorksheet.write(index, 2, cert)
-
-        if cert == "查询失败":
+        if cert == ["查询失败"]:
             failedCount += 1
         else:
             successCount += 1
